48d9f405cc73069904c298d34dbc448ecd46783d
Swapnanil <135691331+swapnanil1@users.noreply.github.com>
2024-10-18 21:19:47 +0530

    Partial Working | Still doesn't calculate next operations

--- js/logic.js
+++ js/logic.js
@@ -1,39 +1,81 @@
-const add = function (n1, ...extras) {
-  for (let n of extras) {
-    n1 += n;
-  }
-  return n1;
-};
+let firstN = undefined;
+let secondN = undefined;
+let ops = undefined;
+const display = document.querySelector("#display");
+const dhistory = document.querySelector(".mainboard .dhistory");
 
-const subtract = function (n1, ...extras) {
-  for (let n of extras) {
-    n1 -= n;
-  }
-  return n1;
-};
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    display.textContent = "";
+    firstN = undefined;
+    secondN = undefined;
+    display.textContent = "";
+    dhistory.textContent = "History: ";
+});
+const output = document.querySelector(".output");
+output.addEventListener("click", () => {
+    secondN = parseFloat(display.textContent);
+    dhistory.textContent += `= ${operate(ops, firstN, secondN)} , `;
+    firstN = operate(ops, firstN, secondN);
+    display.textContent = "";
+    secondN = undefined;
+});
+// i have to calcuate the output every time operator is pressed
+const numbers = document.querySelectorAll(".nums");
+numbers.forEach((e) => {
+    e.addEventListener("click", () => {
+        display.textContent += e.textContent;
+        dhistory.textContent += e.textContent;
+        // if (secondN === undefined){
+        //     secondN = display.textContent;
+        // }
+    });
+});
 
-const sum = function (array) {
-  let sum = 0;
-  for (let n of array) sum = sum + n;
-  return sum;
-};
+const operator = document.querySelectorAll(".operator");
+operator.forEach((e) => {
+    e.addEventListener("click", () => {
+        ops = e.textContent;
+        dhistory.textContent += e.textContent;
 
-const multiply = function (array) {
-  let multi = 1;
-  for (let n of array) {
-    multi *= n;
-  }
-  return multi;
-};
+        secondN = parseFloat(display.textContent);
+        if (firstN !== undefined && secondN !== undefined) {
+            display.textContent = parseFloat(operate(ops, firstN, secondN));
+        }
 
-const power = function (n1, n2) {
-  return Math.pow(n1, n2);
-};
+        firstN = parseFloat(display.textContent);
+        history.textContent = display.textContent;
+        display.textContent = "";
+    });
+});
 
-const factorial = function (n1) {
-  fac = 1;
-  for (let i = 1; i <= n1; i++) {
-    fac = fac * i;
-  }
-  return fac;
-};
+function operate(ops, firstN, secondN) {
+    display.textContent = "";
+    const add = function (n1, n2) {
+        return n1 + n2;
+    };
+
+    const subtract = function (n1, n2) {
+        return n1 - n2;
+    };
+
+    const multiply = function (n1, n2) {
+        return n1 * n2;
+    };
+
+    const divide = function (n1, n2) {
+        return n1 / n2;
+    };
+    if (ops === "+") {
+        result = add(firstN, secondN);
+    } else if (ops === "-") {
+        result = subtract(firstN, secondN);
+    } else if (ops === "X") {
+        result = multiply(firstN, secondN);
+    } else if (ops === "/") {
+        result = divide(firstN, secondN);
+    } else {
+        result = "Error";
+    }
+    return result;
+}
